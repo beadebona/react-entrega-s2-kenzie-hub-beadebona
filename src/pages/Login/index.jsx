7778fcc20717a0592bffac925f6c2aa9eb02fed6
beadebona <beatrizdebona.bd@gmail.com>
2022-04-18 20:28:22 -0300

    feat: Login Page Layout

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,32 @@
+
+import { useHistory } from "react-router-dom"
+import Button from "../../components/Button"
+import Input from "../../components/Input"
+import Logo from "../../components/Logo"
+import Container from "./style"
+
+const Login = ()=>{
+    const history = useHistory()
+
+    return (
+    <>
+        <Logo/>
+        <Container>
+          
+            <form>
+                <h2>Login</h2>
+
+                <Input placeholder="Digite seu nome" label="Nome"/>
+                <Input placeholder="Digite sua senha" type="password" label="Senha"/>
+                <Button text="Entrar"/>
+                {/* <span>Ainda não possui uma conta?</span>
+                <Link>Cadastre-se</Link> */}
+            </form> 
+            <span>Ainda não possui uma conta?</span>  
+            <Button text="Cadastre-se" onClick={() => history.push("/cadastro")}/>
+        </Container>
+    </>
+    )
+}
+
+export default Login
